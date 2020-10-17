39167f6d685d7ed2aee1c9c8ed696d24d8217f36
Thiago-l <thigo.l17@yahoo.com>
2020-10-17 18:04:30 -0300

    adding success page

---
+++ web/src/pages/OrphanageSuccess.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import ImgSuccess from '../images/success.svg';
+import '../styles/pages/orphanage-success.css';
+
+function OrphanageSuccess() {
+  return (
+    <div id="main">
+      <div className="success">
+        <main >
+          <h1>Ebaaa !</h1>
+          <span>
+          O cadastro deu certo e foi enviado ao administrador para ser aprovado. <br />
+              Agora é só esperar :)
+          </span> 
+        </main>
+
+        <img src={ImgSuccess} alt="Success"></img>
+      
+        <Link to="/app" className="back-To-Map">
+        <span>Voltar para o mapa</span>
+        </Link>
+      </div>
+    </div>
+    
+  );
+}
+
+export default OrphanageSuccess;
