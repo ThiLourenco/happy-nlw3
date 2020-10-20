88c87d9d804dafb1d5e1fb59a103110226695326
Thiago-l <thigo.l17@yahoo.com>
2020-10-19 22:09:59 -0300

    login page

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,48 +1,51 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
-import { useHistory } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
 
 import PageLogin from "../components/PageLogin";
 import '../styles/pages/login.css';
 
 
 export default function Login() {
-	// const {goback} = useHistory();
 
 	return (
 		
 	<div id="login">
 		<PageLogin />
-	
-	
-	<aside>	
-		<form className="form-login" >
-			<fieldset>
-			<legend>Fazer login</legend>
-			<div className="input-block">
-				<label htmlFor="email">E-mail</label>
-				<input id="name"/>	
-			</div>
-
-			<div className="input-block">
-				<label htmlFor="password">Senha</label>
-				<input id="name" />
-			</div>
-
-			<div className="remember-me">
-				<input id="checkbox" type="checkbox" name="checked"></input>
-				<label htmlFor="remember">Lembrar-me</label>
-
-				<span>Esqueci a senha</span>
+		
+		<aside>	
+			<form className="form-login" method="POST">
+				<fieldset>
+				<legend>Fazer login</legend>
+				<div className="input-block">
+					<label htmlFor="email">E-mail</label>
+					<input id="name"/>	
 				</div>
+
+				<div className="input-block">
+					<label htmlFor="password">Senha</label>
+					<input id="name" />
+				</div>
+
+				<div className="remember-me">
+					<input id="checkbox" type="checkbox" name="checked"></input>
+					<label htmlFor="remember">Lembrar-me</label>
+
+					<span>Esqueci a senha</span>
+					</div>
+					
+				</fieldset>
 				
-			</fieldset>
+				<button className="button-login" type="submit">
+					Entrar
+				</button>
+			</form>
 			
-			<button className="button-login" type="submit">
-				Entrar
-			</button>
-		</form>
-	</aside>
+			<Link to="/" className="back-home">
+					<FiArrowLeft size={26} color="#15C3D6"  />
+			</Link>
+			
+		</aside>
 	</div>
 	);
 }
